0e7fbfc0c712ce5f9de8fb3203fdb0c60e9cba81
02Naitsirk <iifrostii@outlook.com>
2021-11-02 13:55:41 -0400

    New deviation formula; temporarily remove cheesing corrections

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -4,7 +4,6 @@
 using MathNet.Numerics;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using MathNet.Numerics.RootFinding;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
@@ -47,86 +46,20 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             var currentObject = (OsuDifficultyHitObject)current;
             var currentObjectBase = (OsuHitObject)currentObject.BaseObject;
 
-            var lastObject = Previous.Count > 0 ? (OsuDifficultyHitObject)Previous[0] : null;
-            var secondLastObject = Previous.Count > 1 ? (OsuDifficultyHitObject)Previous[1] : null;
-
-            OsuDifficultyHitObject nextObject = null;
-
-            if (lastObject != null && currentObject.nextObject != null)
-            {
-                nextObject = new OsuDifficultyHitObject(null, currentObject.nextObject, (OsuHitObject)lastObject.BaseObject, currentObjectBase, clockRate);
-            }
-
             double radius = currentObjectBase.Radius;
 
             if (currentObject.BaseObject is Spinner)
                 return 0;
 
-            // Add extra time to deltaTime for cheesing corrections.
-            double extraDeltaTime = 0;
-
-            /*
-             * Correction #1: Early taps.
-             * The player can tap the current note early if the previous deltaTime is greater than the current deltaTime.
-             * This kind of cheesing gives the player extra time to hit the current pattern.
-             * The maximum amount of extra time is the 50 hit window or the time difference, whichever is lower.
-             */
-
-            if (secondLastObject == null)
-            {
-                extraDeltaTime += mehHitWindow;
-            }
-            else
-            {
-                Debug.Assert(lastObject != null, nameof(lastObject) + " != null");
-                double deltaTime = currentObject.StartTime - lastObject.StartTime;
-                double lastDeltaTime = lastObject.StartTime - secondLastObject.StartTime;
-                double timeDifference = lastDeltaTime - deltaTime;
-
-                if (timeDifference > 0)
-                {
-                    extraDeltaTime += Math.Min(mehHitWindow, timeDifference);
-                }
-            }
-
-            /*
-             * Correction #2: Early taps.
-             * The player can tap the current note late if the next deltaTime is greater than the current deltaTime.
-             * This kind of cheesing gives the player extra time to hit the current pattern.
-             * The maximum amount of extra time is the 50 hit window or the time difference, whichever is lower.
-             */
-
-            if (nextObject == null)
-            {
-                extraDeltaTime += mehHitWindow;
-            }
-            else
-            {
-                double nextDeltaTime = nextObject.StartTime - currentObject.StartTime;
-                double deltaTime = currentObject.StartTime - lastObject.StartTime;
-                double timeDifference = nextDeltaTime - deltaTime;
-
-                if (timeDifference > 0)
-                {
-                    extraDeltaTime += Math.Min(mehHitWindow, timeDifference);
-                }
-            }
-
-            extraDeltaTime = Math.Min(mehHitWindow, extraDeltaTime);
-            double effectiveDeltaTime = currentObject.DeltaTime + extraDeltaTime;
-
             // These formulas come from data.
 
-            double difficulty;
-            const double deviation_intercept = 100;
+            const double b = 1.63;
+            const double m = 4.79;
+            double difficulty = b + m * currentObject.JumpDistance / currentObject.DeltaTime;
 
-            if (currentObject.JumpDistance >= 2 * radius)
+            if (currentObject.JumpDistance < 2 * radius)
             {
-                difficulty = (currentObject.JumpDistance + currentObject.TravelDistance + deviation_intercept) / effectiveDeltaTime;
-            }
-            else
-            {
-                difficulty = (currentObject.JumpDistance + currentObject.TravelDistance) * (2 * radius + deviation_intercept) / (2 * radius * effectiveDeltaTime);
+                difficulty *= currentObject.JumpDistance / (2 * radius);
             }
 
             return difficulty / radius;
@@ -179,8 +112,8 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         public override double DifficultyValue()
         {
-            const double guess_lower_bound = 0;
-            const double guess_upper_bound = 1;
+            const double guess_lower_bound = 0.0;
+            const double guess_upper_bound = 2.0;
 
             double expectedHitsMinusThreshold(double skill)
             {
