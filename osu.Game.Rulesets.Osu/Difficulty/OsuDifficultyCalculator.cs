c8a3a17d13068ac82b92804305c250f13e1d4323
02Naitsirk <iifrostii@outlook.com>
2021-11-03 17:37:15 -0400

    Reduce aim scaling

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -20,7 +20,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
 {
     public class OsuDifficultyCalculator : DifficultyCalculator
     {
-        private const double aim_scaling = 6.8;
+        private const double aim_scaling = 6;
         private const double aim_exp = 1.0;
         private const double tap_scaling = 15;
         private const double tap_exp = 0.5;
