abc999d85cc665fa7c5c1030fb84c41068f5200e
02Naitsirk <iifrostii@outlook.com>
2022-01-15 17:57:13 -0500

    Add JSON attributes to difficulty values

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyAttributes.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyAttributes.cs
@@ -1,15 +1,22 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using Newtonsoft.Json;
 using osu.Game.Rulesets.Difficulty;
 
 namespace osu.Game.Rulesets.Osu.Difficulty
 {
     public class OsuDifficultyAttributes : DifficultyAttributes
     {
+        [JsonProperty("aim_difficulty")]
         public double AimStrain { get; set; }
+
+        [JsonProperty("speed_difficulty")]
         public double SpeedStrain { get; set; }
+
+        [JsonProperty("fl_difficulty")]
         public double FlashlightRating { get; set; }
+
         public double ApproachRate { get; set; }
         public double OverallDifficulty { get; set; }
         public double DrainRate { get; set; }
