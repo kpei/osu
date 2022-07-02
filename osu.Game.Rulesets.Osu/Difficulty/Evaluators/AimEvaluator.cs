cb6f6949f5efb321b2425ced831029b5a152ecce
02Naitsirk <iifrostii@outlook.com>
2022-07-02 19:41:09 -0400

    Expand the position function to 2 dimensions; introduce 2D velocity vector

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -4,6 +4,7 @@
 #nullable disable
 
 using System;
+using MathNet.Numerics.LinearAlgebra.Double;
 using MathNet.Numerics.RootFinding;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
@@ -12,8 +13,10 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 {
     public static class AimEvaluator
     {
+        private const double root_finding_accuracy = 1e-4;
+
         /// <summary>
-        /// Evaluates the difficulty of aiming the current object.
+        /// Evaluates the difficulty of successfully aiming at the current object.
         /// </summary>
         public static double EvaluateDifficultyOf(DifficultyHitObject current)
         {
@@ -25,12 +28,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         /// <summary>
         /// Calculates the aim difficulty of the current object for a player with an aim skill of 1.
         /// </summary>
-        /// <param name="current">
-        /// The current object.
-        /// </param>
-        /// <returns>
-        /// The aim difficulty of the current object.
-        /// </returns>
         private static double aimDifficultyOf(DifficultyHitObject current)
         {
             var osuCurrObj = (OsuDifficultyHitObject)current;
@@ -38,95 +35,113 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         }
 
         /// <summary>
-        /// Calculates the coordination difficulty of the current object.
-        /// The coordination difficulty is the reciprocal of half of the amount of time the player spends in the note.
+        /// Calculates the coordination difficulty of the current object, defined as the reciprocal of half of the amount of time the player spends in the note.
         /// </summary>
-        /// <param name="current">
-        /// The current object.
-        /// </param>
-        /// <returns>
-        /// The coordination difficulty of the current object.
-        /// </returns>
         private static double coordinationDifficultyOf(DifficultyHitObject current)
         {
+            var osuPrevObj = (OsuDifficultyHitObject)current.Previous(0);
             var osuCurrObj = (OsuDifficultyHitObject)current;
             var osuNextObj = (OsuDifficultyHitObject)current.Next(0);
 
-            double timeInCurrentNote = 0;
+            double timeInNote = 0;
 
-            // Determine the position function from the previous note to the current note.
-            // Then, determine when the position function equals LazyJumpDistance - 1, which is the time that the player enters the note.
-            // If this number is subtracted from DeltaTime, we get the amount of time the cursor is in the note as it moves from the previous note to the current note.
+            // Determine the amount of time the cursor is within the current circle as it moves from the previous circle.
+            if (osuPrevObj != null)
+            {
+                // This distance must be computed because sliders aren't being taken into account.
+                double realSquaredDistance = Math.Pow(osuCurrObj.NormalizedX - osuPrevObj.NormalizedX, 2) + Math.Pow(osuCurrObj.NormalizedY - osuPrevObj.NormalizedY, 2);
 
-            if (osuCurrObj.LazyJumpDistance > 1)
-            {
-                double currentPositionFunctionWithRoot(double time) => positionFunction(osuCurrObj.LazyJumpDistance, osuCurrObj.StrainTime, 0, 0, time) - (osuCurrObj.LazyJumpDistance - 1);
-                double timeSpentInNoteWhileEntering = osuCurrObj.StrainTime - Brent.FindRoot(currentPositionFunctionWithRoot, 0, osuCurrObj.StrainTime, 1e-4);
-                timeInCurrentNote += timeSpentInNoteWhileEntering;
-            }
-            else
-            {
                 // If the current and previous objects are overlapped by 50% or more, just add the DeltaTime of the current object.
-                timeInCurrentNote += osuCurrObj.StrainTime;
-            }
-
-            // As the player leaves the current note and moves on to the next note, the player spends some time in the current note as well.
-            // We should therefore take into account this amount of time.
-            // This time is calculated and added to the amount of time spent in the current note.
-
-            if (osuNextObj != null)
-            {
-                if (osuNextObj.LazyJumpDistance > 1)
+                if (realSquaredDistance <= 1)
                 {
-                    double nextPositionFunctionWithRoot(double time) => positionFunction(osuNextObj.LazyJumpDistance, osuNextObj.StrainTime, 0, 0, time) - 1;
-                    double timeSpentInNoteWhileExiting = Brent.FindRoot(nextPositionFunctionWithRoot, 0, osuNextObj.StrainTime, 1e-4);
-                    timeInCurrentNote += timeSpentInNoteWhileExiting;
+                    timeInNote += osuCurrObj.StrainTime;
                 }
                 else
                 {
-                    timeInCurrentNote += osuNextObj.StrainTime;
+                    double xComponent(double t) => positionVectorOf(current, t)[0];
+                    double yComponent(double t) => positionVectorOf(current, t)[1];
+
+                    // The circle located at (X, Y) with radius r can be described by the equation (x - X)^2 + (y - Y)^2 = r^2.
+                    // We can find when the position function intersects the circle by substituting xComponent(t) into x and yComponent(t) into y,
+                    // subtracting r^2 from both sides of the equation, and then solving for t.
+                    // Because the positions are normalized with respect to the radius, r^2 = 1.
+                    double root(double t) => Math.Pow(xComponent(t) - osuCurrObj.NormalizedX, 2) + Math.Pow(yComponent(t) - osuCurrObj.NormalizedY, 2) - 1;
+
+                    double timeEnterNote = Brent.FindRoot(root, 0, osuCurrObj.StrainTime, root_finding_accuracy);
+                    timeInNote += osuCurrObj.StrainTime - timeEnterNote;
                 }
             }
-            else
+
+            // Determine the amount of time the cursor is within the current circle as it moves toward the next circle.
+            if (osuNextObj != null)
             {
-                timeInCurrentNote += 200;
+                double realSquaredDistance = Math.Pow(osuNextObj.NormalizedX - osuCurrObj.NormalizedX, 2) + Math.Pow(osuNextObj.NormalizedY - osuCurrObj.NormalizedY, 2);
+
+                if (realSquaredDistance <= 1)
+                {
+                    timeInNote += osuNextObj.StrainTime;
+                }
+                else
+                {
+                    double xComponent(double t) => positionVectorOf(osuNextObj, t)[0];
+                    double yComponent(double t) => positionVectorOf(osuNextObj, t)[1];
+
+                    double root(double t) => Math.Pow(xComponent(t) - osuCurrObj.NormalizedX, 2) + Math.Pow(yComponent(t) - osuCurrObj.NormalizedY, 2) - 1;
+
+                    double timeExitNote = Brent.FindRoot(root, 0, osuNextObj.StrainTime, root_finding_accuracy);
+                    timeInNote += timeExitNote;
+                }
             }
 
-            // If the amount of time spent in the note is t, then this is approximately the same as a hit window of ± t / 2.
-            double hitWindow = timeInCurrentNote / 2;
+            if (timeInNote == 0)
+                return 0;
 
-            return 1 / hitWindow;
+            return 2 / timeInNote;
         }
 
         /// <summary>
-        /// Gives the cursor's position at time <paramref name="t"/>. Returns 0 when <paramref name="t"/> = 0, and returns <paramref name="distance"/> when <paramref name="t"/> = <paramref name="deltaTime"/>.
-        /// The cursor begins with an velocity of <paramref name="initialVelocity"/> at <paramref name="t"/> = 0, and ends with a velocity of
-        /// <paramref name="finalVelocity"/> at <paramref name="t"/> = <paramref name="deltaTime"/>.
+        /// A path from the previous <paramref name="hitObject"/> to the current <paramref name="hitObject"/> is generated, which the cursor is assumed to follow.
+        /// This function returns the cursor's position at any time <paramref name="t"/>, where t can range from 0 to this <paramref name="hitObject"/>'s DeltaTime.
+        /// At <paramref name="t"/> = 0, the function returns the coordinates of the previous object.
+        /// At <paramref name="t"/> = DeltaTime, the function returns the coordinates of the current object.
         /// </summary>
-        /// <param name="distance">
-        /// How far the cursor moves.
-        /// </param>
-        /// <param name="deltaTime">
-        /// How much time the cursor has to move.
-        /// </param>
-        /// <param name="initialVelocity">
-        /// The cursor's velocity when <paramref name="t"/> = 0.
-        /// </param>
-        /// <param name="finalVelocity">
-        ///The cursor's velocity when <paramref name="t"/> = <paramref name="deltaTime"/>.
-        /// </param>
-        /// <param name="t">
-        /// Any time between 0 and <paramref name="deltaTime"/>.
-        /// </param>
-        /// <returns>
-        /// The cursor's position at time <paramref name="t"/>.
-        /// </returns>
-        private static double positionFunction(double distance, double deltaTime, double initialVelocity, double finalVelocity, double t)
+        private static DenseVector positionVectorOf(DifficultyHitObject hitObject, double t)
         {
-            double c1 = (10 * distance - deltaTime * (4 * finalVelocity + 6 * initialVelocity)) / Math.Pow(deltaTime, 3);
-            double c2 = (15 * distance - deltaTime * (7 * finalVelocity + 8 * initialVelocity)) / Math.Pow(deltaTime, 4);
-            double c3 = (6 * distance - deltaTime * (3 * finalVelocity + 3 * initialVelocity)) / Math.Pow(deltaTime, 5);
-            return initialVelocity * t + c1 * t * t * t - c2 * t * t * t * t + c3 * t * t * t * t * t;
+            var osuPrevObj = (OsuDifficultyHitObject)hitObject.Previous(0);
+            var osuCurrObj = (OsuDifficultyHitObject)hitObject;
+
+            var previousVelocityVector = generateVelocityVectorOf(osuPrevObj);
+            var currentVelocityVector = generateVelocityVectorOf(osuCurrObj);
+
+            double ax = osuPrevObj.NormalizedX;
+            double bx = osuCurrObj.NormalizedX;
+
+            double ay = osuPrevObj.NormalizedY;
+            double by = osuCurrObj.NormalizedY;
+
+            double dt = osuCurrObj.StrainTime;
+
+            double ix = previousVelocityVector[0]; // Velocity along the x direction of the previous object
+            double iy = previousVelocityVector[1]; // Velocity along the y direction of the previous object
+            double fx = currentVelocityVector[0]; // Velocity along the x direction of the current object
+            double fy = currentVelocityVector[1]; // Velocity along the y direction of the current object
+
+            double xComponent = (-Math.Pow(t - dt, 3) * (6 * t * t + 3 * t * dt + dt * dt) * ax + Math.Pow(t, 3) * (6 * t * t - 15 * t * dt + 10 * dt * dt) * bx - t * (t - dt) * dt * (t * t * (3 * t - 4 * dt) * fx + Math.Pow(t - dt, 2) * (3 * t + dt) * ix))
+                                / Math.Pow(dt, 5);
+            double yComponent = (-Math.Pow(t - dt, 3) * (6 * t * t + 3 * t * dt + dt * dt) * ay + Math.Pow(t, 3) * (6 * t * t - 15 * t * dt + 10 * dt * dt) * by - t * (t - dt) * dt * (t * t * (3 * t - 4 * dt) * fy + Math.Pow(t - dt, 2) * (3 * t + dt) * iy))
+                                / Math.Pow(dt, 5);
+
+            double[] positionVector = { xComponent, yComponent };
+            return new DenseVector(positionVector);
+        }
+
+        /// <summary>
+        /// Returns the velocity vector of the <paramref name="hitObject"/> at the time the <paramref name="hitObject"/> is located at.
+        /// </summary>
+        private static DenseVector generateVelocityVectorOf(DifficultyHitObject hitObject)
+        {
+            double[] velocityVector = { 0.0, 0.0 };
+            return new DenseVector(velocityVector);
         }
     }
 }
